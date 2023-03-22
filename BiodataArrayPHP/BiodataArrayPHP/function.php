e2f4d09175e9656d5e7da2c16576496ef4b26473
alyaiz <alyaizzahzalfa@gmail.com>
2023-03-22 13:00:34 +0700

    menambahkan array ke biodata PHP

---
+++ BiodataArrayPHP/BiodataArrayPHP/function.php
@@ -0,0 +1,28 @@
+<?php 
+
+// Function untuk bisa menghitung umur otomatis
+function age()
+{
+    $tanggal_lahir = new DateTime("2002-12-03");
+    $sekarang = new DateTime("today");
+    if ($tanggal_lahir > $sekarang) {
+        $tahun = "0";
+        $bulan = "0";
+        $tanggal = "0";
+    }
+    $tahun = $sekarang->diff($tanggal_lahir)->y;
+    $bulan = $sekarang->diff($tanggal_lahir)->m;
+    $tanggal = $sekarang->diff($tanggal_lahir)->d;
+    echo $tahun." tahun ".$bulan." bulan ".$tanggal." hari";
+} 
+
+//array untuk menampilkan biodata
+$array = [
+    "nama" => "Alya Izzah",
+    "nohp" => "+6285232247252",
+    "hobi" => "Basket",
+    "suku" => "Jawa",
+    "alamat" => "Tuban, Jatim",
+    "agama" => "Islam",
+    "status" => "Belum Menikah"
+];
